01f5674bafd4947be6059c925974f5176400d23d
anirud645 <anirudh268409@gmail.com>
2025-06-17 20:29:01 +0530

    w1:DP&P:E1: Singelton Pattern Implementation

--- Week_1/designpatterns/singletonpatternexample/Logger.java
+++ Week_1/designpatterns/singletonpatternexample/Logger.java
@@ -1,4 +1,4 @@
-package designpatterns.singleton;
+package designpatterns.singletonpatternexample;
 
 public class Logger {
 
--- Week_1/designpatterns/singletonpatternexample/Test.java
+++ Week_1/designpatterns/singletonpatternexample/Test.java
@@ -1,4 +1,4 @@
-package designpatterns.singleton;
+package designpatterns.singletonpatternexample;
 
 public class Test {
     public static void main(String args[]) {
